781b763e95701fda6f38704794cb95550d5142b8
mi1an <jadavmilan009@gmail.com>
2022-05-26 22:22:40 +0530

    changes

---
+++ JavaScript/function.js
@@ -0,0 +1,23 @@
+function hello() {
+  console.log("Hello!");
+}
+
+hello();
+
+function sum() {
+  let a = 4;
+  let b = 2;
+
+  console.log(a + b);
+}
+
+sum();
+
+function funWithReturn() {
+  let a = 5;
+  let b = 2;
+
+  return a + b;
+}
+
+console.log(funWithReturn());
---
+++ JavaScript/loop.js
@@ -0,0 +1,7 @@
+//  Table of
+let num = 3;
+
+for (let i = 1; i <= 10; i++) {
+  res = i * num;
+  document.write(num + " * " + i + " = " + res, "<br>");
+}
